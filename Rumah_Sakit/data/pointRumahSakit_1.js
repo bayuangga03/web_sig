b9b2bba4895bae8bb941a943f00416a4973f24c6
bayuangga03 <108395829+bayuangga03@users.noreply.github.com>
2022-07-06 04:20:47 +0700

    Add files via upload

---
+++ Rumah_Sakit/data/pointRumahSakit_1.js
@@ -0,0 +1,14 @@
+var json_pointRumahSakit_1 = {
+"type": "FeatureCollection",
+"name": "pointRumahSakit_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "kecamatan": "Jepara", "Rumah_skt": "RSU R.A. Kartini", "alamat": "Jl. K.H Wahid Hasyim Jepara", "No. Telp": "(0291) 593286" }, "geometry": { "type": "Point", "coordinates": [ 110.67697336104095, -6.572173656936264 ] } },
+{ "type": "Feature", "properties": { "id": "2", "kecamatan": "Jepara", "Rumah_skt": "RS Sultan Hadlirin", "alamat": "Jl. Jepara-Bangsri KM.3,Kuwasen,Jepara", "No. Telp": "(0291) 591507" }, "geometry": { "type": "Point", "coordinates": [ 110.665562113977359, -6.557803938411732 ] } },
+{ "type": "Feature", "properties": { "id": "3", "kecamatan": "Jepara", "Rumah_skt": "RS Graha Husada", "alamat": "Jl. MH. Thamrin No. 14, Jepara", "No. Telp": "(0291) 592067" }, "geometry": { "type": "Point", "coordinates": [ 110.664294197636963, -6.569215185475331 ] } },
+{ "type": "Feature", "properties": { "id": "4", "kecamatan": "Jepara", "Rumah_skt": "RS PKU Aisyiyah Jepara", "alamat": "Jl. Brigjen Katamso No. 18, Panggang I, Panggang, Kec. Jepara, Kabupaten, Jepara", "No. Telp": "082227152801" }, "geometry": { "type": "Point", "coordinates": [ 110.669365862998561, -6.566256714014398 ] } },
+{ "type": "Feature", "properties": { "id": "5", "kecamatan": "Mayong", "Rumah_skt": "RS PKU Muhammadiyah Mayong, Jepara", "alamat": "Jl. Pegadaian Mayong No.12, Gloge, Mayong Lor, Kabupaten Jepara", "No. Telp": "02914256500" }, "geometry": { "type": "Point", "coordinates": [ 110.758479249789673, -6.729205095694783 ] } },
+{ "type": "Feature", "properties": { "id": "6", "kecamatan": "Keling", "Rumah_skt": "RSUD Rehatta", "alamat": "Jl. Raya Kelet-Jepara No.KM 33, Karang Anyar, Kelet, Kec. Keling", "No. Telp": "0291579002" }, "geometry": { "type": "Point", "coordinates": [ 110.881847509710568, -6.477143327223296 ] } },
+{ "type": "Feature", "properties": { "id": "7", "kecamatan": "Donorojo", "Rumah_skt": "Rumah Sakit Kusta Donorojo", "alamat": "Jalan greja gitj, Sumber Telu, Banyumanis, Kec. Donorojo, Kabupaten Jepara", "No. Telp": "02915792002" }, "geometry": { "type": "Point", "coordinates": [ 110.887616529059386, -6.411909031509724 ] } }
+]
+}
